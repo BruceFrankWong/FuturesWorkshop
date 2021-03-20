b42223e8ba6c74acc28ea69681aec4bf82142dcd
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-03-20 16:41:14 +0800

    Correct typo.

--- FuturesWorkshop/ui/main_window.py
+++ FuturesWorkshop/ui/main_window.py
@@ -6,6 +6,7 @@ __author__ = 'Bruce Frank Wong'
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 from .main_window_ui import Ui_MainWindow
+from .dialog_preference import DialogPreference
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -14,4 +15,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self._ui = Ui_MainWindow()
         self._ui.setupUi(self)
 
+        self._ui.actionPreference.triggered.connect(self.show_dialog_preference)
         self._ui.actionQuit.triggered.connect(QtWidgets.qApp.quit)
+
+    def show_dialog_preference(self):
+        dialog_preference = DialogPreference(self)
+        dialog_preference.show()
