2dcd85393341ebae7cd27a6b9b09bcbe1679f649
Bruce Fank Wong <bruce.frank.wong@gmail.com>
2021-03-23 14:18:16 +0800

    Update module interface.

--- FuturesWorkshop/ui/__init__.py
+++ FuturesWorkshop/ui/__init__.py
@@ -4,3 +4,11 @@ __author__ = 'Bruce Frank Wong'
 
 
 from .main_window import MainWindow
+from .dialog_preference import DialogPreference
+from .dialog_test_server import DialogTestServer
+
+__all__ = [
+    'MainWindow',
+    'DialogPreference',
+    'DialogTestServer',
+]
